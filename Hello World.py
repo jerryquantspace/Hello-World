f15c400026c503081b45db0bdd6f89d94c641d81
Jeremiah <jeremiahmohammedazn@gmail.com>
2025-11-11 19:22:42 -0400

    Add files via upload

---
+++ Hello World.py
@@ -0,0 +1,19 @@
+input ("Press enter to execute") 
+import time
+import sys
+text = ("hello world!" 
+"this is my first programme")
+for char in text:
+    sys.stdout.write(char)
+    sys.stdout.flush()
+    time.sleep(0.1)
+print()
+import time
+import sys
+text = ("Press enter to close")
+for char in text:
+    sys.stdout.write(char)
+    sys.stdout.flush()
+    time.sleep(0.1)
+print ()
+input ()
